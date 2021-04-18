0793c39f380e53bd5bc08fa06d6e6e782acca44a
jbfink <john.fink@gmail.com>
2021-04-18 11:52:00 -0400

    sgp30 setup.

--- code.py
+++ code.py
@@ -7,4 +7,11 @@ import adafruit_bitmap_font
 import adafruit_display_text
 import adafruit_magtag.magtag as MagTag
 
+# setup sgp30 sensor
+i2c = busio.I2C(board.SCL, board.SDA, frequency=100000)
+sgp30 = adafruit_sgp30.Adafruit_SGP30(i2c)
+sgp30.iaq_init()
+sgp30.set_iaq_baseline(secrets.secrets['eCO2'], secrets.secrets['TVOC'])
+
 print("Hello World!")
+
