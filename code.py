dd366ff862f7db7a2e451e0eee6203cb7c1b0746
jbfink <john.fink@gmail.com>
2021-04-18 11:45:28 -0400

    import modules in code.py

--- code.py
+++ code.py
@@ -1,1 +1,10 @@
+# core modules import
+import time, board, busio, secrets
+# adafruit add-on modules
+import adafruit_sgp30
+import adafruit_portalbase
+import adafruit_bitmap_font
+import adafruit_display_text
+import adafruit_magtag.magtag as MagTag
+
 print("Hello World!")
