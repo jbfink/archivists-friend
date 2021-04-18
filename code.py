72aff4ff2b810e1c867843674170a019eb4f5063
jbfink <john.fink@gmail.com>
2021-04-18 11:55:11 -0400

    Whoops, imported magtag library wrong, plus magtag object setup.

--- code.py
+++ code.py
@@ -5,13 +5,16 @@ import adafruit_sgp30
 import adafruit_portalbase
 import adafruit_bitmap_font
 import adafruit_display_text
-import adafruit_magtag.magtag as MagTag
+from adafruit_magtag.magtag import MagTag
 
 # setup sgp30 sensor
 i2c = busio.I2C(board.SCL, board.SDA, frequency=100000)
 sgp30 = adafruit_sgp30.Adafruit_SGP30(i2c)
 sgp30.iaq_init()
 sgp30.set_iaq_baseline(secrets.secrets['eCO2'], secrets.secrets['TVOC'])
+
+# setup MagTag object:
+magtag = MagTag()
 
 print("Hello World!")
 
