991e2b115e6ce6592aab35feb34ed57c8dee1333
jbfink <john.fink@gmail.com>
2021-04-18 20:58:05 -0400

    More libraries (that I might not need; will assess later), plus commented-out stanza for network connect.

--- code.py
+++ code.py
@@ -1,6 +1,7 @@
 # core modules import
-import time, board, busio, secrets
+import time, board, busio, secrets, wifi, ipaddress, ssl, socketpool, terminalio, displayio
 # adafruit add-on modules
+import adafruit_requests
 import adafruit_sgp30
 import adafruit_portalbase
 import adafruit_bitmap_font
@@ -15,6 +16,12 @@ sgp30.set_iaq_baseline(secrets.secrets['eCO2'], secrets.secrets['TVOC'])
 
 # setup MagTag object:
 magtag = MagTag()
+# since network connections take time, this is commented out until I actually
+# need the network.
+#try:
+#    magtag.network.connect()
+#except:
+#    print("Can't connect to access point!")
 
 print("Hello World!")
 
